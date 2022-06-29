1c04004f97e7a0626e3f5556e3ecfa1a7dc0f19a
jane <CROSSHAIR@MSI>
2022-06-29 17:34:19 -0500

    Ciudad created

---
+++ votoelectronico-domain/src/main/java/ec/edu/insteclrg/domain/Ciudad.java
@@ -0,0 +1,34 @@
+package ec.edu.insteclrg.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+//@Table(name = "provincia")
+public class Ciudad {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(unique = true, nullable = false)
+	private long id;
+	@Column(nullable = false)
+	private String name;
+
+	// relacion OneToMany
+//	@ManyToOne
+//	@JoinColumn(name = "provincia_id")
+//	private Provincia provincia;
+
+}
