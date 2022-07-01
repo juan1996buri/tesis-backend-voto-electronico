993f3e06bd51d1468a90bc22f7f12d02218cf807
Pedro Ordoñez <pedroo382@gmail.com>
2022-06-30 19:06:38 -0500

    Sexo Domain

---
+++ votoelectronico-domain/src/main/java/ec/edu/insteclrg/domain/Sexo.java
@@ -0,0 +1,22 @@
+package ec.edu.insteclrg.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Sexo {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		@Column(updatable = false, nullable = false)
+		private long id;
+		
+		@Column (nullable = false)
+		private String name;
+}
