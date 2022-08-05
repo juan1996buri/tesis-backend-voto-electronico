6bcb406b5a2ce6de60bdcab361b67b329e86ec4a
juan1996buri <juan98buri@gmail.com>
2022-08-05 16:28:27 -0500

    added cross origin

--- votoelectronico-app/src/main/java/ec/edu/insteclrg/app/api/v1/JuntaController.java
+++ votoelectronico-app/src/main/java/ec/edu/insteclrg/app/api/v1/JuntaController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import ec.edu.insteclrg.domain.Junta;
 import ec.edu.insteclrg.dto.JuntaDTO;
 import ec.edu.insteclrg.service.crud.JuntaService;
 
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping(value = "/api/v1.0/junta")
 public class JuntaController {
